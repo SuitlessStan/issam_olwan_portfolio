398f95c6d2f09b840f34fc6cce63d426f6b77d53
Issam Olwan <lands.of.48@gmail.com>
2023-07-20 13:14:56 +0300

    edit error alert message

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -72,15 +72,13 @@ export default function ContactMe() {
             </div>
           )}
           {showError && (
-            <div className="message-progress">
-              <div
-                className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-                role="alert">
-                <strong className="font-bold">Holy smokes!</strong>
-                <span className="block sm:inline">
-                  Something went wrong while sending the message
-                </span>
-              </div>
+            <div
+              className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+              role="alert">
+              <strong className="font-bold">Oh no!</strong>
+              <span className="block sm:inline">
+                Something went wrong while sending the message
+              </span>
             </div>
           )}
         </div>
